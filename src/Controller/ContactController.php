934352b1550748ccb2f92887c58abc856e73dd38
unknown <you@example.com>
2022-11-08 18:08:18 +0100

    mise en forme de la page d'acceuil et certains dependances du projet

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AcceuilController extends AbstractController
+class ContactController extends AbstractController
 {
-    #[Route('/', name: 'app_acceuil')]
+    #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-        return $this->render('acceuil/index.html.twig', [
-            'controller_name' => 'AcceuilController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 }
